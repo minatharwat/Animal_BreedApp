1fa1352e28d16d7b82c40bf6a0998e1b90a64993
mina <minatharwat00x@yahoo.com>
2018-06-17 02:50:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/animal_breedapp/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.animal_breedapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ${Mina} on 16/06/2018.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        init_data();
+    }
+
+    public  void  init_data(){
+    }
+
+}
---
+++ app/src/main/java/com/example/user/animal_breedapp/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.example.user.animal_breedapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        intializeControls();
+
+       LoginWithFb();
+
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn){
+
+            Intent i=new Intent(this,HomeActivity.class);
+            startActivity(i);
+        }
+
+    }
+
+
+    private void intializeControls(){
+        loginButton=findViewById(R.id.login_button);
+        callbackManager=CallbackManager.Factory.create();
+
+
+    }
+    private void LoginWithFb(){
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        Toast.makeText(LoginActivity.this, " hi"+loginResult.getAccessToken()+loginResult.toString(), Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
